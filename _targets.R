7ab1c6a3412bd50de948aae119e2007d1657f164
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2023-04-27 14:52:01 -0400

    adding foreign keys

--- _targets.R
+++ _targets.R
@@ -26,14 +26,18 @@ wahisdb <- tar_plan(
   # Process into epi_event and outbreak table
   tar_target(wahis_tables, create_wahis_tables(wahis_extract), cue = tar_cue(run_cue)),
 
+  # TODO clean disease name
+
   # Add to database
   tar_target(wahis_tables_in_db, add_data_to_db(data = wahis_tables,
                                                 primary_key_lookup = c("wahis_epi_events" = "epi_event_id_unique",
                                                                        "wahis_outbreaks" = "report_outbreak_species_id_unique"),
                                                 db_branch = db_branch), cue = tar_cue(run_cue)),
 
-  # TODO clean disease name
-  # TODO set foreign keys
+  # Set foreign keys
+  tar_target(wahis_tables_in_db_with_foreign_keys,
+             set_foreign_keys(wahis_tables_in_db,
+                              db_branch = db_branch), cue = tar_cue(run_cue)),
 
   # Read schema (this can also come from the sharepoint pull)
   tar_target(schema_extract_file, "wahis-extracts/Field_description.xlsx", format = "file", repository = "local", cue = tar_cue("thorough")),
@@ -43,7 +47,7 @@ wahisdb <- tar_plan(
 
   tar_target(schema_in_db, add_data_to_db(data = list("schema_tables" = schema_tables,
                                                       "schema_fields" = schema_fields),
-                                          primary_key_lookup = c("schema_table" = "table",
+                                          primary_key_lookup = c("schema_tables" = "table",
                                                                  "schema_fields" = "id"),
                                           db_branch), cue = tar_cue("thorough")),
 
